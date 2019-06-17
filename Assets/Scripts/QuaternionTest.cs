6de81f79c7ff2a461fc94bb2a9049bcf0f7b778a
LitLov3 <912778422@qq.com>
2019-06-17 23:46:06 +0800

    init

---
+++ Assets/Scripts/QuaternionTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionTest : MonoBehaviour {
+
+    [Range(0f, 1f)]
+    public float lerpValue;
+
+    public Vector3 target;
+    public Quaternion rot, diff;
+
+    private void Start()
+    {
+        rot = transform.rotation;
+        diff = transform.rotation * Quaternion.Inverse(Quaternion.identity);
+    }
+
+    private void Update()
+    {
+
+        transform.rotation = Quaternion.Slerp(rot, Quaternion.Inverse(diff), lerpValue);
+    }
+}
---
+++ Assets/Scripts/WaterControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterControl : MonoBehaviour {
+
+    public float lerpSpeed = 5f;
+    public float dampingSpeed = 0.3f;
+    private float lerpValue;
+    private float maxAngle_diff = -1f;
+    private Quaternion maxQuat_diff = Quaternion.identity;
+
+    private void Update()
+    {
+        Quaternion currentRot = transform.rotation;
+        Quaternion diff = currentRot * Quaternion.Inverse(Quaternion.identity);
+        float angle_diff = Quaternion.Angle(currentRot, Quaternion.identity);
+        if (angle_diff > maxAngle_diff)
+        {
+            maxAngle_diff = angle_diff;
+            maxQuat_diff = diff;
+        }
+
+        maxQuat_diff = Quaternion.Slerp(maxQuat_diff, Quaternion.identity, dampingSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, Quaternion.Inverse(maxQuat_diff), lerpSpeed * Time.deltaTime);
+
+        //print("CurrentRot " + currentRot + " diff * id " + diff * Quaternion.identity + " diff " + diff + " diff inverse " + Quaternion.Inverse(diff));
+    }
+}
